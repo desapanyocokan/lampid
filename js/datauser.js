93a5bb7c312a6893a5411a55be74f5c5a3865e01
desapanyocokan <desapanyocokan24@gmail.com>
2025-12-03 10:58:45 +0700

    Initial commit of website files

---
+++ js/datauser.js
@@ -0,0 +1,30 @@
+window.users = [
+    // Ketua RW (23 orang)
+    { username: 'rw1', password: 'rw123', role: 'Ketua RW 1' },
+    { username: 'rw2', password: 'rw123', role: 'Ketua RW 2' },
+    { username: 'rw3', password: 'rw123', role: 'Ketua RW 3' },
+    { username: 'rw4', password: 'rw123', role: 'Ketua RW 4' },
+    { username: 'rw5', password: 'rw123', role: 'Ketua RW 5' },
+    { username: 'rw6', password: 'rw123', role: 'Ketua RW 6' },
+    { username: 'rw7', password: 'rw123', role: 'Ketua RW 7' },
+    { username: 'rw8', password: 'rw123', role: 'Ketua RW 8' },
+    { username: 'rw9', password: 'rw123', role: 'Ketua RW 9' },
+    { username: 'rw10', password: 'rw123', role: 'Ketua RW 10' },
+    { username: 'rw11', password: 'rw123', role: 'Ketua RW 11' },
+    { username: 'rw12', password: 'rw123', role: 'Ketua RW 12' },
+    { username: 'rw13', password: 'rw123', role: 'Ketua RW 13' },
+    { username: 'rw14', password: 'rw123', role: 'Ketua RW 14' },
+    { username: 'rw15', password: 'rw123', role: 'Ketua RW 15' },
+    { username: 'rw16', password: 'rw123', role: 'Ketua RW 16' },
+    { username: 'rw17', password: 'rw123', role: 'Ketua RW 17' },
+    { username: 'rw18', password: 'rw123', role: 'Ketua RW 18' },
+    { username: 'rw19', password: 'rw123', role: 'Ketua RW 19' },
+    { username: 'rw20', password: 'rw123', role: 'Ketua RW 20' },
+    { username: 'rw21', password: 'rw123', role: 'Ketua RW 21' },
+    { username: 'rw22', password: 'rw123', role: 'Ketua RW 22' },
+    { username: 'rw23', password: 'rw123', role: 'Ketua RW 23' },
+    // Petugas Registrasi Desa
+    { username: 'registrasi', password: 'reg123', role: 'Petugas Registrasi' },
+    // Kepala Desa
+    { username: 'kepala', password: 'kep123', role: 'Kepala Desa' }
+];
